d48d5c7306717d5a1febc70a11042d9f21b632ce
fabiodaemon <44199988+fabiodaemon@users.noreply.github.com>
2019-05-07 00:24:37 -0300

    Initial commit

---
+++ Scripts/script.js
@@ -0,0 +1,20 @@
+function openModal() {
+   
+    document.getElementById('creditos').style.display = "block";
+
+}; 
+
+function closeModal() {
+    
+    document.getElementById('creditos').style.display = "none";
+
+};
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
